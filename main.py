fbd6c18e828db5fd534ce3de86d12592c4241996
kevinthomasoc <50811018+kevinthomasoc@users.noreply.github.com>
2022-02-12 22:37:27 -0800

    Add files via upload
    Added GUI rather than running it out of command line.

--- main.py
+++ main.py
@@ -1,28 +1,65 @@
+import tkinter
 import pyperclip
-
+from tkinter import *
+import time
+#Clipboard Function
 clipboard = {}
-while True:
-    action = input("What would you like to do? (Read, Load, or Save)")
-    if action.lower() == "read":
-        if len(clipboard) < 1:
-            print("You have nothing in your clipboard.")
-        else:
-            for key in clipboard:
-                print(key, clipboard[key])
-    elif action.lower() == "save":
-        message = input("What do you want to save?")
-        key = input("What do you want to save it as? (Make sure you remember this!)")
-        clipboard[key] = message
-        print("It has been saved!")
-    elif action.lower() == "load":
-        if len(clipboard) < 1:
-            print("You have nothing in your clipboard.")
-        else:
-            key = (input("What key would you like to access?")).lower()
-            print(clipboard[key] + " has been copied to your clipboard!")
-            pyperclip.copy(clipboard[key])
+msg = "Hi"
+
+if msg.lower() == "load":
+    if len(clipboard) < 1:
+        print("You have nothing in your clipboard.")
     else:
-        print("Please enter a valid input.")
+        key = (input("What key would you like to access?")).lower()
+        print(clipboard[key] + " has been copied to your clipboard!")
+        pyperclip.copy(clipboard[key])
 
 
+def readFunction():
+     if len(clipboard) < 1:
+         print("You have nothing in your clipboard.")
+     else:
+         for key in clipboard:
+             label = Label(win, text=key+" "+ clipboard[key])
+             label.pack()
 
+
+def saveFunction():
+    clipboard[key.get()] = message.get()
+    message.delete(0, 'end')
+    key.delete(0, 'end')
+    label = Label(win, text="Your item has been saved!")
+    label.pack()
+
+def loadFunction():
+    label = Label(win, text=clipboard[userkey.get()])
+    label.pack()
+
+
+#Frontend
+#Window
+win = Tk()
+win.title("MultiClipboard")
+win.geometry("700x400")
+#Read Button
+readButton = tkinter.Button(win, text = "Read", command = readFunction)
+readButton.pack()
+
+
+#Save Button + TextBoxes
+message = Entry(win, width = 50, bg = "blue", fg = "white", borderwidth = 5)
+message.pack()
+key = Entry(win, width = 30, bg = "red", fg = "white", borderwidth = 5)
+key.pack()
+saveButton = tkinter.Button(win, text = "Save", command = saveFunction)
+saveButton.pack()
+
+
+#Load Button
+userkey = Entry(win, width = 50, bg = "green", fg = "white", borderwidth = 5)
+userkey.pack()
+loadButton = tkinter.Button(win, text = "Load", command = loadFunction)
+loadButton.pack()
+
+#Window Loop
+win.mainloop()
